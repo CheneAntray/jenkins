009b1712a3e5ba04c2ad303555eb5560720377ba
acorus <CheneAntray@163.com>
2020-04-29 19:37:41 +0800

    修改.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@ git pull
 npm install --registry=https://registry.npm.taobao.org/ && npm run build
 
 # 删除容器
-docker rm -f zhangsan &> /dev/null
+docker rm -f acorus &> /dev/null
 
 #启动容器
 docker run -d --restart=on-failure:5\
     -p 8090:80 \
     -v $PWD/dist:/usr/share/nginx/html \
-    --name zhangsan nginx
+    --name acorus nginx
 
 
